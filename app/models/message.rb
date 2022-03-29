cbb35e2a9feca8906a3f711eb01bb6db2f1fb750
izureco <freestyleaerial@gmail.com>
2022-03-29 12:42:49 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  # MessageテーブルとActive Storageテーブルの画像ファイルを紐づけた
+  # has_one_attached :ファイル名 と書けば、"モデル名.ファイル名"でファイルにアクセスできる。
+  # 例) Messageモデルのimageファイル : Message.imageと書けば、その画像の持つパラメータのキーにもできる。
+  has_one_attached :image
 
   validates :content, presence: true
 end
