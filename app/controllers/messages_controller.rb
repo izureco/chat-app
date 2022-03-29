25038b0d4d29627d4441b38dd398613f524819c6
izureco <freestyleaerial@gmail.com>
2022-03-29 11:00:24 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    # @messageで、チャットを保存していく。
     @message = Message.new
+    # roomとmessageは親子関係なので、indexアクションに飛んできたとき、paramsにroom_idが格納されている。
     @room = Room.find(params[:room_id])
+    # @room.messages : チャットルームに紐づいている全てのメッセージは、user_idと紐づいている。
+    # includes(:user) : 一度の参照で、全ての紐付けを獲得することで、N+1問題を解消している。
+    @messages = @room.messages.includes(:user)
   end
 
   def create
+    # Roomレコードから、現在の部屋情報を引き出す
     @room = Room.find(params[:room_id])
+    # その部屋情報に紐づく、チャット情報(ユーザーid含む)を新たに保存する。
     @message = @room.messages.new(message_params)
     if  @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
