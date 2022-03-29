62d6ecd1a9156e33a39e1cb549e22b8e05680722
izureco <freestyleaerial@gmail.com>
2022-03-29 14:24:46 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,5 +6,14 @@ class Message < ApplicationRecord
   # 例) Messageモデルのimageファイル : Message.imageと書けば、その画像の持つパラメータのキーにもできる。
   has_one_attached :image
 
-  validates :content, presence: true
+  # ▼は、2つの項目がtrueのとき、validate(制限)するという意味
+  ## ❶ :content, presence: true -> "チャット"が空である
+  ## ❷ unless: :was_attatched?  -> "画像"が添付されていない(was_attachedの戻り値がfalse)
+  # つまり、❶が満たされていても、❷で画像が添付されていれば投稿は可能。逆もまた然り
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
