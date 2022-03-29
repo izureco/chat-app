e9da11d28fe513caa77335bd67477f9ed3fc63d5
izureco <freestyleaerial@gmail.com>
2022-03-29 11:30:05 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,9 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  # roomの削除(destroyアクション)を行うと、子のmessagesも一緒に削除する
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  has_many :messages
+  # roomの削除(destroyアクション)を行うと、子のmessagesも一緒に削除する
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 
