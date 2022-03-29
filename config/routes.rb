31ff15acec5bfb49ae90acc3cf625821fb94d4a8
izureco <freestyleaerial@gmail.com>
2022-03-29 09:36:48 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create, :index]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:create, :index]
+  end
   root to: "rooms#index"
 end
